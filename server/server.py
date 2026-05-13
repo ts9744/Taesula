0b0e05ada4fecd29505d0c5ea9c81c0f19c03eeb
02LMJ <alswo3745@naver.com>
2026-05-13 20:17:09 +0900

    [feat] 서버 파일 실행 방식 개선

--- server/server.py
+++ server/server.py
@@ -318,3 +318,7 @@ def update_robot_status(current_x: int, current_y: int, status: str):
         "current_y": current_y,
         "status": status
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
