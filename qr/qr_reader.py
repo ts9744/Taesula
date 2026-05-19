95d587492722c06f4e02a2e58eb6ec368d8882d9
02LMJ <alswo3745@naver.com>
2026-05-19 21:59:35 +0900

    [fix] 서버 URL 링크 수정

--- qr/qr_reader.py
+++ qr/qr_reader.py
@@ -2,7 +2,7 @@ import cv2
 from pyzbar.pyzbar import decode
 import requests
 
-SERVER_URL = "http://127.0.0.1:8000"
+SERVER_URL = "http://taesula.local:8000"
 
 def read_qr_from_camera():
     cap = cv2.VideoCapture(0)
